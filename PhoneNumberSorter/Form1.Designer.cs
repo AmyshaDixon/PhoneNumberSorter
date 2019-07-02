fc35e60580dbd4cd6e0504fd9ab5db4ed0ad7299
Amy Dixon <amysha.dixon@gmail.com>
2019-07-02 14:51:16 -0700

    Add project files.

---
+++ PhoneNumberSorter/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace PhoneNumberSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDelete = new System.Windows.Forms.Label();
+            this.lblCompare = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.tbDelete = new System.Windows.Forms.TextBox();
+            this.tbCompare = new System.Windows.Forms.TextBox();
+            this.btnCompare = new System.Windows.Forms.Button();
+            this.btnParse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDelete
+            // 
+            this.lblDelete.AutoSize = true;
+            this.lblDelete.Location = new System.Drawing.Point(112, 270);
+            this.lblDelete.Name = "lblDelete";
+            this.lblDelete.Size = new System.Drawing.Size(351, 32);
+            this.lblDelete.TabIndex = 0;
+            this.lblDelete.Text = "Select a file to delete from:";
+            // 
+            // lblCompare
+            // 
+            this.lblCompare.AccessibleDescription = "";
+            this.lblCompare.AutoSize = true;
+            this.lblCompare.Location = new System.Drawing.Point(112, 378);
+            this.lblCompare.Name = "lblCompare";
+            this.lblCompare.Size = new System.Drawing.Size(350, 32);
+            this.lblCompare.TabIndex = 1;
+            this.lblCompare.Text = "Select a file to compare to:";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(941, 246);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(270, 73);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Browse";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // tbDelete
+            // 
+            this.tbDelete.Enabled = false;
+            this.tbDelete.Location = new System.Drawing.Point(474, 264);
+            this.tbDelete.MinimumSize = new System.Drawing.Size(400, 0);
+            this.tbDelete.Name = "tbDelete";
+            this.tbDelete.Size = new System.Drawing.Size(400, 38);
+            this.tbDelete.TabIndex = 3;
+            // 
+            // tbCompare
+            // 
+            this.tbCompare.Enabled = false;
+            this.tbCompare.Location = new System.Drawing.Point(474, 378);
+            this.tbCompare.MinimumSize = new System.Drawing.Size(400, 0);
+            this.tbCompare.Name = "tbCompare";
+            this.tbCompare.Size = new System.Drawing.Size(400, 38);
+            this.tbCompare.TabIndex = 4;
+            // 
+            // btnCompare
+            // 
+            this.btnCompare.Location = new System.Drawing.Point(941, 360);
+            this.btnCompare.Name = "btnCompare";
+            this.btnCompare.Size = new System.Drawing.Size(270, 73);
+            this.btnCompare.TabIndex = 5;
+            this.btnCompare.Text = "Browse";
+            this.btnCompare.UseVisualStyleBackColor = true;
+            this.btnCompare.Click += new System.EventHandler(this.BtnCompare_Click);
+            // 
+            // btnParse
+            // 
+            this.btnParse.ForeColor = System.Drawing.Color.Green;
+            this.btnParse.Location = new System.Drawing.Point(528, 528);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(276, 103);
+            this.btnParse.TabIndex = 6;
+            this.btnParse.Text = "Parse";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.BtnParse_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 784);
+            this.Controls.Add(this.btnParse);
+            this.Controls.Add(this.btnCompare);
+            this.Controls.Add(this.tbCompare);
+            this.Controls.Add(this.tbDelete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblCompare);
+            this.Controls.Add(this.lblDelete);
+            this.Name = "Form1";
+            this.Text = "Phone Number Sorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDelete;
+        private System.Windows.Forms.Label lblCompare;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox tbDelete;
+        private System.Windows.Forms.TextBox tbCompare;
+        private System.Windows.Forms.Button btnCompare;
+        private System.Windows.Forms.Button btnParse;
+    }
+}
+
---
+++ PhoneNumberSorter/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneNumberSorter
+{
+    public partial class Form1 : Form
+    {
+        // Create variables
+        string FILE_CONTENTS_DELETE = string.Empty;
+        string FILE_CONTENTS_COMPARE = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Allows user to choose a .txt file to delete numbers from
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFile = new OpenFileDialog())
+            {
+                openFile.InitialDirectory = "c:\\"; // Automatically directs user to c:\\ path when pop-up opens
+                openFile.Filter = "txt files|*.txt"; // Restricts user to uploading only .txt files
+                openFile.RestoreDirectory = true; // Resets director to original path
+
+                if(openFile.ShowDialog() == DialogResult.OK)
+                {
+                    // First display file path in tbDelete for user to see
+                    tbDelete.Text = openFile.SafeFileName;
+
+                    // Retrieve contents of file
+                    var fileStream = openFile.OpenFile();
+
+                    // Store file contents into FILE_CONTENTS_DELETE variable
+                    using(StreamReader fileReader = new StreamReader(fileStream))
+                    {
+                        FILE_CONTENTS_DELETE = fileReader.ReadToEnd(); // Makes one big string
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Allows user to choose a .txt file to compare the first list to
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnCompare_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileAgain = new OpenFileDialog())
+            {
+                openFileAgain.InitialDirectory = "c:\\"; 
+                openFileAgain.Filter = "txt files|*.txt"; 
+                openFileAgain.RestoreDirectory = true; 
+
+                if (openFileAgain.ShowDialog() == DialogResult.OK)
+                {
+                    tbCompare.Text = openFileAgain.SafeFileName;
+
+                    var fileStream2 = openFileAgain.OpenFile();
+
+                    using (StreamReader fileReader2 = new StreamReader(fileStream2))
+                    {
+                        FILE_CONTENTS_COMPARE = fileReader2.ReadToEnd(); 
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compares the first list to the second and deletes any differing numbers
+        /// from list 1. Produces a new list in a .txt file for the user to save
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnParse_Click(object sender, EventArgs e)
+        {
+            // Create array lists
+            ArrayList deletableList = new ArrayList();
+            ArrayList comparableList = new ArrayList();
+
+            // Separate list data
+            string[] deletableLines = FILE_CONTENTS_DELETE.Split('\n');
+            string[] comparableLines = FILE_CONTENTS_COMPARE.Split('\n');
+
+            // Store line data (numbers) into arrays
+            for (int i = 0; i < deletableLines.Length; i++)
+            {
+                deletableList.Add(deletableLines[i].Trim());
+            }
+
+            for (int i = 0; i < comparableLines.Length; i++)
+            {
+                comparableList.Add(deletableLines[i].Trim());
+            }
+
+            // Compare lists and delete differing numbers from first list
+            for(int i = 0; i < deletableList.Count; i++)
+            {
+                if(!comparableList.Contains(deletableList[i]))
+                {
+                    deletableList.Remove(deletableList[i]);
+                }
+            }
+
+            // Provide user with a file to save
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            saveFile.FileName = "newListOfNumbers.txt";
+            saveFile.Filter = "txt files|*.txt";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter streamWriter = new StreamWriter(saveFile.OpenFile());
+
+                for(int i = 0; i < deletableList.Count; i++)
+                {
+                    streamWriter.WriteLine(deletableList[i]);
+                }
+
+                streamWriter.Dispose();
+                streamWriter.Close();
+            }
+        }
+    }
+}
