cf81c5581ccac9460da83d8420c69a9d9aa3504d
Work Amy <Amy.Dixon@symetra.com>
2019-07-03 12:17:01 -0700

    Refactored the code and deleted some redundancy

--- PhoneNumberSorter/Form1.cs
+++ PhoneNumberSorter/Form1.cs
@@ -31,27 +31,7 @@ namespace PhoneNumberSorter
         /// <param name="e"></param>
         private void BtnDelete_Click(object sender, EventArgs e)
         {
-            using(OpenFileDialog openFile = new OpenFileDialog())
-            {
-                openFile.InitialDirectory = "c:\\"; // Automatically directs user to c:\\ path when pop-up opens
-                openFile.Filter = "txt files|*.txt"; // Restricts user to uploading only .txt files
-                openFile.RestoreDirectory = true; // Resets director to original path
-
-                if(openFile.ShowDialog() == DialogResult.OK)
-                {
-                    // First display file path in tbDelete for user to see
-                    tbDelete.Text = openFile.SafeFileName;
-
-                    // Retrieve contents of file
-                    var fileStream = openFile.OpenFile();
-
-                    // Store file contents into FILE_CONTENTS_DELETE variable
-                    using(StreamReader fileReader = new StreamReader(fileStream))
-                    {
-                        FILE_CONTENTS_DELETE = fileReader.ReadToEnd(); // Makes one big string
-                    }
-                }
-            }
+            ImportUserFile(true);
         }
 
         /// <summary>
@@ -61,26 +41,55 @@ namespace PhoneNumberSorter
         /// <param name="e"></param>
         private void BtnCompare_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog openFileAgain = new OpenFileDialog())
+            ImportUserFile(false);
+        }
+
+        /// <summary>
+        /// Opens a dialog for the user to select files from their
+        /// computer
+        /// </summary>
+        /// <param name="fileToggle"> Represents which variable to store the file
+        /// information into; true for FILE_CONTENTS_DELETE and false for
+        /// FILE_CONTENTS_COMPARE</param>
+        private void ImportUserFile(bool fileToggle)
+        {
+            using (OpenFileDialog openFile = new OpenFileDialog())
             {
-                openFileAgain.InitialDirectory = "c:\\"; 
-                openFileAgain.Filter = "txt files|*.txt"; 
-                openFileAgain.RestoreDirectory = true; 
+                openFile.InitialDirectory = "c:\\"; // Automatically directs user to c:\\ path when pop-up opens
+                openFile.Filter = "txt files|*.txt"; // Restricts user to uploading only .txt files
+                openFile.RestoreDirectory = true; // Resets director to original path
 
-                if (openFileAgain.ShowDialog() == DialogResult.OK)
+                if (openFile.ShowDialog() == DialogResult.OK)
                 {
-                    tbCompare.Text = openFileAgain.SafeFileName;
-
-                    var fileStream2 = openFileAgain.OpenFile();
-
-                    using (StreamReader fileReader2 = new StreamReader(fileStream2))
+                    // First display file path in tbDelete for user to see
+                    if (fileToggle)
                     {
-                        FILE_CONTENTS_COMPARE = fileReader2.ReadToEnd(); 
+                        tbDelete.Text = openFile.SafeFileName;
+                    }
+                    else
+                    {
+                        tbCompare.Text = openFile.SafeFileName;
+                    }
+
+                    // Retrieve contents of file
+                    var fileStream = openFile.OpenFile();
+
+                    // Store file contents into FILE_CONTENTS_DELETE variable
+                    using (StreamReader fileReader = new StreamReader(fileStream))
+                    {
+                        if (fileToggle)
+                        {
+                            FILE_CONTENTS_DELETE = fileReader.ReadToEnd(); // Makes one big string
+                        }
+                        else
+                        {
+                            FILE_CONTENTS_COMPARE = fileReader.ReadToEnd();
+                        }
                     }
                 }
             }
         }
-
+        
         /// <summary>
         /// Compares the first list to the second and deletes any differing numbers
         /// from list 1. Produces a new list in a .txt file for the user to save
@@ -89,44 +98,70 @@ namespace PhoneNumberSorter
         /// <param name="e"></param>
         private void BtnParse_Click(object sender, EventArgs e)
         {
-            // Create array lists
-            List<Int64> deletableList = new List<Int64>();
-            List<Int64> comparableList = new List<Int64>();
+            // Create lists
+            List<long> deletableList = new List<long>();
+            List<long> comparableList = new List<long>();
 
             // Separate list data
             string[] deletableLines = FILE_CONTENTS_DELETE.Split('\n');
             string[] comparableLines = FILE_CONTENTS_COMPARE.Split('\n');
 
             // Store line data (numbers) into arrays
-            for (int i = 0; i < deletableLines.Length; i++)
-            {
-                string simplifyLine = Regex.Replace(deletableLines[i], @"\t|\n|\r", "").Trim();
-
-                deletableList.Add(Convert.ToInt64(simplifyLine));
-            }
-
-            for (int i = 0; i < comparableLines.Length; i++)
-            {
-                string simplifyLine = Regex.Replace(comparableLines[i], @"\t|\n|\r", "").Trim();
-
-                comparableList.Add(Convert.ToInt64(simplifyLine));
-            }
+            LineDataToArray(deletableList, deletableLines);
+            LineDataToArray(comparableList, comparableLines);
 
             // Compare lists and delete differing numbers from first list
-            for(int i = deletableList.Count - 1; i >= 0; i--)
+            CompareLists(deletableList, comparableList);
+
+            // Provide user with a file to save
+            SaveNewFile(deletableList);
+        }
+
+        /// <summary>
+        /// Sepparates a given list of lines into tokens and
+        /// stores them in a given array
+        /// </summary>
+        /// <param name="mainList"></param>
+        /// <param name="lineList"></param>
+        private static void LineDataToArray(List<long> mainList, string[] lineList)
+        {
+            for (int i = 0; i < lineList.Length; i++)
+            {
+                string simplifyLine = Regex.Replace(lineList[i], @"\t|\n|\r", "").Trim();
+
+                mainList.Add(Convert.ToInt64(simplifyLine));
+            }
+        }
+
+        /// <summary>
+        /// Compares the deletableList to the comparableList and removed
+        /// differing numbers that DO NOT beging with the area code 619
+        /// </summary>
+        /// <param name="deletableList"></param>
+        /// <param name="comparableList"></param>
+        private static void CompareLists(List<long> deletableList, List<long> comparableList)
+        {
+            for (int i = deletableList.Count - 1; i >= 0; i--)
             {
                 //If deletable list has a number different than comparable                
                 if (!(comparableList.Contains(deletableList[i])))
                 {
                     // and DOES NOT start with 619, delete it
-                    if (Convert.ToInt64(deletableList[i]) / 10000000 != 619)
+                    if (deletableList[i] / 10000000 != 619)
                     {
                         deletableList.Remove(deletableList[i]);
                     }
                 }
             }
+        }
 
-            // Provide user with a file to save
+        /// <summary>
+        /// Pops open a dialog for the user to save a newly written
+        /// file of numbers to their computer
+        /// </summary>
+        /// <param name="deletableList"></param>
+        private static void SaveNewFile(List<long> deletableList)
+        {
             SaveFileDialog saveFile = new SaveFileDialog();
 
             saveFile.FileName = "newListOfNumbers.txt";
@@ -136,7 +171,7 @@ namespace PhoneNumberSorter
             {
                 StreamWriter streamWriter = new StreamWriter(saveFile.OpenFile());
 
-                for(int i = 0; i < deletableList.Count; i++)
+                for (int i = 0; i < deletableList.Count; i++)
                 {
                     streamWriter.WriteLine(deletableList[i]);
                 }
@@ -144,6 +179,6 @@ namespace PhoneNumberSorter
                 streamWriter.Dispose();
                 streamWriter.Close();
             }
-        }
+        }        
     }
 }
